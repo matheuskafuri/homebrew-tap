be396fd64c992ebbba4ab8fef91a740900a9cb71
Matheus Ramos Kafuri <matheus10.rk@gmail.com>
2026-02-24 00:14:39 -0300

    Update formula to v0.1.0 release tarball

--- Formula/devnews.rb
+++ Formula/devnews.rb
@@ -1,8 +1,8 @@
 class Devnews < Formula
   desc "TUI engineering blog aggregator"
   homepage "https://github.com/matheuskafuri/devnews"
-  url "https://github.com/matheuskafuri/devnews/archive/refs/heads/main.tar.gz"
-  version "0.1.0"
+  url "https://github.com/matheuskafuri/devnews/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "77b54c935a03aef6a31ee466a3e334023f7ad190250b70c02cada96fd8f84fe9"
   license "MIT"
 
   depends_on "go" => :build
@@ -11,7 +11,7 @@ class Devnews < Formula
     ldflags = %W[
       -s -w
       -X main.version=#{version}
-      -X main.commit=#{tap.user}
+      -X main.commit=HEAD
       -X main.date=#{time.iso8601}
     ]
     system "go", "build", *std_go_args(ldflags:)
